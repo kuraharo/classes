8f23ecc5c908f867257fdebbf7e6564b32435c9a
kuraharo <kuraharo@mail.ru>
2025-03-07 19:37:43 +0300

    Add files via upload

---
+++ parentheses.py
@@ -0,0 +1,28 @@
+def valid(item1,item2):
+    dictionary={"[":"]","{":"}","(":")","]":None,")":None,"}":None,"0":None}
+    if item2==dictionary[str(item1)]:
+        return True
+    return False
+
+
+string_input=input()
+flag=1
+string_output=""
+mat_to_del=[]
+cnt=0
+while len(string_input)>0 and flag==1:
+    cnt+=1
+    flag=0
+    for index in range(len(string_input)-1):
+        current_simvol=string_input[index]
+        next_simvol=string_input[index+1]
+        if valid(current_simvol,next_simvol):
+            string_input=string_input[:index]+"00"+string_input[index+2:]
+            flag=1
+    string_input=string_input.replace("0","")
+    mat_to_del=[]
+    print(string_input)
+if flag==1:
+    print(True)
+else:
+    print(False)
